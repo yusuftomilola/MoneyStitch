b32e8f322cbe1efd57d22938ac547d9945867bd7
Yusuf Falade <faladeyusuf54@gmail.com>
2025-09-20 11:44:14 +0100

    added the auth and the users

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //GLOBAL VALIDATION
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  //GLOBAL SERIALIZATION
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
